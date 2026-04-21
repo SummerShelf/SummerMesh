244b3a2c83ff2c8f4d8fdef71dfd373d29677ca6
SummerShelf <timo.shuvalov@gmail.com>
2026-04-21 22:00:50 +0200

    added mode switch

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ unsigned char page=0;
 int readValue=0;
 unsigned char lastbuttonpress=8;
 unsigned char type=0;
+bool retranslatormode = false;
+bool recievemode = true;
 const char* menuelements[]={"reciever mode","retranslator mode","transmit"};
 struct __attribute__((__packed__)) packagesingle{
   uint8_t kind;
@@ -38,8 +40,14 @@ void loop() {
   } 
   else if (readValue > 650 && readValue < 870) {
     lastbuttonpress = 1;
-    if (page ==2){
+    if (page ==2 && type==0){
       sendsingle(2, "Hello world, This is Summer. Test of new lora chip. This UTF-8 String is compacted as much as possible");
+    }
+    if (page==0 && type==0){
+      recievemode=!recievemode;
+    }
+    if (page==1 && type==0){
+      retranslatormode=!retranslatormode;
     }
   } 
   else if (readValue > 350 && readValue < 600) {
@@ -61,7 +69,7 @@ void loop() {
       u8g2.setCursor(10, (20*(i+1))-10);
       if (i==page){
         u8g2.setDrawColor(1);
-        u8g2.drawBox(0, (i * 20), 128, 20);
+        u8g2.drawBox(0, (i * 20), 108, 20);
         u8g2.setDrawColor(0);
       }
       else{
@@ -70,6 +78,16 @@ void loop() {
     u8g2.print(menuelements[i]);
     }
   }
+ u8g2.setDrawColor(1);
+ u8g2.drawDisc(116, 10, 9);
+ u8g2.drawDisc(116, 30, 9);
+ u8g2.setDrawColor(0);
+ if (recievemode==false){
+  u8g2.drawDisc(116, 10, 8);
+ }
+ if (retranslatormode==false){
+  u8g2.drawDisc(116, 30, 8);
+ }
  u8g2.sendBuffer();
  Serial.println(readValue);
  delay(10);
