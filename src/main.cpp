74e549062a32741d085c1f2263ed3a5ee8b0e109
SummerShelf <timo.shuvalov@gmail.com>
2026-04-23 21:52:27 +0200

    added recieve function (not tested)

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 #include <RadioLib.h>
 #include <string.h>
 
+#define lenretbuffer 10
+
 SX1262 radio = new Module(8, 3, 5, 4);
 U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 1, /* data=*/ 0, /* reset=*/ U8X8_PIN_NONE);
 unsigned char page=0;
 int readValue=0;
+uint32_t retranslatedmessageids[lenretbuffer];
+int head=0;
 unsigned char lastbuttonpress=8;
 unsigned char type=0;
 bool retranslatormode = false;
@@ -14,7 +18,15 @@ bool recievemode = true;
 bool recieving = false;
 bool recieveflag = false;
 const char* menuelements[]={"reciever mode","retranslator mode","transmit"};
-struct __attribute__((__packed__)) packagesingle{
+struct mainrecivepackage{//package for recieving data
+  uint8_t kind;
+  uint8_t networkid;
+  uint32_t messageid;
+  uint32_t recieveid;
+  uint8_t hops;
+  char message[];
+};
+struct __attribute__((__packed__)) packagesingle{//package for transmiting data with type: single (no return, no connection)
   uint8_t kind;
   uint8_t networkid;
   uint32_t messageid;
@@ -23,7 +35,8 @@ struct __attribute__((__packed__)) packagesingle{
   char message[]; //I don't know how long message will be
 };
 
-void sendsingle(uint32_t targetId, const char* text);
+int sendsingle(uint32_t targetId, const char* text);
+int recieve();
 
 void setflag(){
   recieveflag=true;
@@ -38,6 +51,8 @@ void setup() {
 }
 
 void loop() {
+  if(recieveflag){
+  }
   if (recievemode && !recieving){
     radio.startReceive();
     recieving=true;
@@ -108,22 +123,43 @@ void loop() {
  delay(10);
 }
 
-void sendsingle(uint32_t targetid, const char* message){
+int sendsingle(uint32_t targetid, const char* message, uint8_t sethops, uint32_t setnetwork){
     uint8_t msglen=strlen(message);
     uint8_t buffer[sizeof(packagesingle)+msglen];//creating buffer with size of struct + message
-    struct packagesingle* pkgs = (struct packagesingle*)buffer;
-    pkgs -> kind = (uint8_t)0;
-    pkgs -> networkid = (uint32_t)10;
+    struct packagesingle* pkgs = (struct packagesingle*)buffer;//using same pointer
+    pkgs -> kind = (uint8_t)1;
+    pkgs -> networkid = setnetwork;
     pkgs -> messageid = (uint32_t)20;
-    pkgs -> recieveid = (uint32_t)targetid;
-    pkgs -> hops = (uint8_t)2;
+    pkgs -> recieveid = targetid;
+    pkgs -> hops = sethops;
 
     memcpy(pkgs -> message ,message,msglen);
     int state = radio.transmit(buffer, sizeof(buffer));
     if (state == RADIOLIB_ERR_NONE){
       Serial.println("sended");
+      return 0;
     }
     else{
       Serial.println(state);
+      return 1;
     }
-}   
+}
+
+int recieve(){
+  recieveflag=false;
+  size_t len = radio.getPacketLength();
+  uint8_t* rawrecievedata= new uint8_t[len];
+  radio.readData(rawrecievedata, len);
+  if (*rawrecievedata==(uint8_t)1){
+    struct packagesingle* recievepackage = (struct packagesingle*)rawrecievedata;
+    rawrecievedata[len-1]='\0';
+    if (retranslatormode && recievepackage->hops > 0){
+      if (sendsingle(recievepackage->recieveid,recievepackage->message,(recievepackage->hops)-1, recievepackage->networkid)==0){
+        head++;
+        head%=(lenretbuffer-1);
+        retranslatedmessageids[head]=recievepackage->messageid;
+      }
+    }
+  }
+  delete[] rawrecievedata;
+};
